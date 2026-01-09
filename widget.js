0261276ac11a86ec1d17066b4f9e615c9c8c2fb2
stratsmh <samuel7holm@gmail.com>
2026-01-09 16:38:21 -0600

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,8 +1,8 @@
 const box = document.createElement("div")
 box.innerText = "Hello from my widget!"
 box.style.position = "fixed"
-box.style.top = "20px"
-box.style.left = "20px"
+box.style.bottom = "20px"
+box.style.right = "20px"
 box.style.padding = "12px"
 box.style.background = "black"
 box.style.color = "white"
